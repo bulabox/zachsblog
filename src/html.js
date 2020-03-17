e44b9b7855fa4fb4022e30e36b365f795b242f33
bulabox <61368307+bulabox@users.noreply.github.com>
2020-03-17 18:04:19 -0400

    Updated for New React Component for google
    Google Adsense

--- src/html.js
+++ src/html.js
@@ -1,33 +1,38 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-export default function HTML(props) {
-  return (
-    <html {...props.htmlAttributes}>
-      <head>
-        {props.headComponents}
-      </head>
-      <body {...props.bodyAttributes}>
-        {props.preBodyComponents}
-        <noscript key="noscript" id="gatsby-noscript">
-          This app works best with JavaScript enabled.
-        </noscript>
-        <div
-          key={`body`}
-          id="___gatsby"
-          dangerouslySetInnerHTML={{ __html: props.body }}
-        />
-        {props.postBodyComponents}
-      </body>
-    </html>
-  )
+export default class HTML extends React.Component {
+    render() {
+        const ads = process.env.NODE_ENV === 'production' &&
+            <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" /> ;
+        return (
+            <html {...props.htmlAttributes}>
+            <head>
+                {ads && ads}
+                {props.headComponents}<title/>
+            </head>
+            <body {...props.bodyAttributes}>
+            {props.preBodyComponents}
+            <noscript key="noscript" id="gatsby-noscript">
+                This app works best with JavaScript enabled.
+            </noscript>
+            <div
+                key={`body`}
+                id="___gatsby"
+                dangerouslySetInnerHTML={{__html: props.body}}
+            />
+            {props.postBodyComponents}
+            </body>
+            </html>
+        )
+    }
 }
 
-HTML.propTypes = {
-  htmlAttributes: PropTypes.object,
-  headComponents: PropTypes.array,
-  bodyAttributes: PropTypes.object,
-  preBodyComponents: PropTypes.array,
-  body: PropTypes.string,
-  postBodyComponents: PropTypes.array,
-}
+    HTML.propTypes = {
+        htmlAttributes: PropTypes.object,
+        headComponents: PropTypes.array,
+        bodyAttributes: PropTypes.object,
+        preBodyComponents: PropTypes.array,
+        body: PropTypes.string,
+        postBodyComponents: PropTypes.array,
+    };
